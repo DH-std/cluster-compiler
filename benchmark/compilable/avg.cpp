462f090e684638123c28dc32e582c5e12a1c2abe
Dili Hu <dillyw@eecs583a.eecs.umich.edu>
2016-12-03 12:26:08 -0500

    switch to cycle

---
+++ benchmark/compilable/avg.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+int main() {
+  int i = 2;
+  vector<unsigned long long> ts;
+  unsigned long long t;
+
+  while (cin >> t) {
+    ts.push_back(t);
+  }
+
+  sort(ts.begin(), ts.end());
+  assert(ts.size() > i);
+
+  unsigned long long sum = 0;
+  t = ts[i];
+
+  for (int j = ts.size() - i - 1; j > i; --j) {
+    sum += ts[j] - t;
+  }
+
+  t += sum / (ts.size() - i - i - 1);
+  cout << t << endl;
+
+  return 0;
+}
--- benchmark/compilable/com.sh
+++ benchmark/compilable/com.sh
@@ -15,13 +15,20 @@ opt $seq < $infile.bc  > $infile.opt.bc
 llc $infile.opt.bc -o $infile.opt.s
 gcc -I . -o $infile.opt.exe $infile.opt.s $infile/${infile}_d.c $infile/${infile}_c.c support.c
 
-set +e
+set +x +e
 rm time.output
-set -e
 
-./$infile.opt.exe
+for i in {1..12}
+do
+  ./$infile.opt.exe
+done
+mv time.output time.input
 
-set +x
+g++ -o avg.exe avg.cpp
+./avg.exe < time.input > time.output
+cat time.output
+
 GREEN='\033[0;32m'
 NC='\033[0m'
 echo -e "${GREEN}success!${NC}"
+echo "output in file time.output"
