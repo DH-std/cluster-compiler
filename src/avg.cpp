3e1ac3619e41cb1563176a0e735f2f00e0b86af7
Dili Hu <dillyw@eecs583a.eecs.umich.edu>
2016-12-04 20:58:38 -0500

    modified cycle count

--- src/avg.cpp
+++ src/avg.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  int i = 2;
+  //int i = 2;
   vector<unsigned long long> ts;
   unsigned long long t;
 
@@ -14,17 +14,18 @@ int main() {
   }
 
   sort(ts.begin(), ts.end());
-  assert(ts.size() > i);
+  //assert(ts.size() > i);
+  assert(!ts.empty());
 
-  unsigned long long sum = 0;
-  t = ts[i];
+  //unsigned long long sum = 0;
+  //t = ts[i];
 
-  for (int j = ts.size() - i - 1; j > i; --j) {
-    sum += ts[j] - t;
-  }
+  //for (int j = ts.size() - i - 1; j > i; --j) {
+    //sum += ts[j] - t;
+  //}
 
-  t += sum / (ts.size() - i - i - 1);
-  cout << t << endl;
+  //t += sum / (ts.size() - i - i - 1);
+  cout << ts[0] << endl;
 
   return 0;
 }
