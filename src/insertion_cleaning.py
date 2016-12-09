397cbbc0ede3601eea38708848c982029ad33f80
Lian Mo <molian@Lians-MacBook-Air.local>
2016-12-08 22:09:52 -0500

    insertion-clean not tested

---
+++ src/insertion_cleaning.py
@@ -0,0 +1,32 @@
+import pickle, subprocess, fitness, sys, os
+import cleaning_algo, insertion
+
+def insertion_clean_method(fnc):
+
+    inputfile = open("../output/reduced_space/" + function_name, 'rb')
+    passlist = pickle.load(inputfile)
+    inputfile.close()
+    mid_seq, mid_fit = insertion.insert_method(fnc, passlist, 1)
+
+    best_seq, best_fit = cleaning_algo.clean_method(fnc, mid_seq)
+    return best_seq, best_fit
+
+
+def main(argv):
+    if len(argv) < 2:
+        print "error"
+        return 0
+    function_name = argv[1]
+
+    best_seq, best_fit = insertion_clean_method(function_name, passlist)
+
+    ans = {"seq": best_seq, "fitness": best_fit}
+
+    outputname = '../output/insertion-clean/' + function_name + '.pkl'
+    outputfile = open(outputname, 'wb')
+    pickle.dump(ans, outputfile)
+    outputfile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
