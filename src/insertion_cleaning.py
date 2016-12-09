84df1c37501ce572c9dcc7d0a22e1ba9ae8065a1
Lian Mo <molian@Lians-MacBook-Air.local>
2016-12-08 22:30:23 -0500

    insertion-cleaning

--- src/insertion_cleaning.py
+++ src/insertion_cleaning.py
@@ -3,10 +3,15 @@ import cleaning_algo, insertion
 
 def insertion_clean_method(fnc):
 
-    inputfile = open("../output/reduced_space/" + function_name, 'rb')
-    passlist = pickle.load(inputfile)
+    inputfile = open("../output/reduced_space/" + fnc, 'rb')
+    reduced_space = pickle.load(inputfile)
     inputfile.close()
-    mid_seq, mid_fit = insertion.insert_method(fnc, passlist, 1)
+
+    inputfile = open("full_space.pkl", 'rb')
+    full_space = pickle.load(inputfile)
+    inputfile.close()
+
+    mid_seq, mid_fit = insertion.insert_method(fnc, full_space, 1, reduced_space)
 
     best_seq, best_fit = cleaning_algo.clean_method(fnc, mid_seq)
     return best_seq, best_fit
@@ -18,7 +23,7 @@ def main(argv):
         return 0
     function_name = argv[1]
 
-    best_seq, best_fit = insertion_clean_method(function_name, passlist)
+    best_seq, best_fit = insertion_clean_method(function_name)
 
     ans = {"seq": best_seq, "fitness": best_fit}
 
