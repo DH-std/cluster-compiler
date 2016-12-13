af56d8e1fd1a7cbcb7aecfd1f6f0bba8fb9dd627
Ubuntu <ubuntu@ip-172-31-46-174.us-west-2.compute.internal>
2016-12-13 03:54:12 +0000

    New runDNA

--- src/runDNA.sh
+++ src/runDNA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in "dsp_autocor" "dsp_dotprod" "img_fdct_8x8" "dsp_neg3"
+for file in "img_yc_demux_le16"
 do
   echo "$file"
   rm "../output/time/$file"
@@ -29,7 +29,7 @@ do
   echo "clean reduced: $duration" >> "../output/time/$file"
   echo "clean-insertion"
   SECONDS=0
-  #python clean_insertion.py $file
+  python cic.py $file
   duration=$SECONDS
   echo "clean-insertion: $duration" >> "../output/time/$file"
   echo "insertion-clean"
