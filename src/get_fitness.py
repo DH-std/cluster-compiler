3d7ea45faaed78a4809f9c4f7fd9e9d76d962178
Ubuntu <ubuntu@ip-172-31-22-163.us-west-2.compute.internal>
2016-12-14 18:02:59 +0000

    Update get_fitness.py

--- src/get_fitness.py
+++ src/get_fitness.py
@@ -1,10 +1,9 @@
 import pickle, os
 
 def main():
-
-	filenames = os.listdir('.')
+	filenames = sorted(os.listdir('.'))
 	for filename in filenames:
-        	if filename != "test" and filename != 'get_fitness.py':
+        	if filename != "test" and filename != 'get_fitness.py' and filename != 'img_idct_8x8_12q4':
 			inputfile = open('./' + filename, 'rb')
 			p = pickle.load(inputfile)
 			print filename, p['fitness']
