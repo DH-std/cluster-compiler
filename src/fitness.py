70fafddcdce6ce1df2b3522a6a425b1cc92d00d5
Ubuntu <ubuntu@ip-172-31-22-112.ec2.internal>
2016-12-14 02:37:23 +0000

    change fitness evaluation

--- src/fitness.py
+++ src/fitness.py
@@ -2,16 +2,17 @@ import os
 import subprocess
 
 def evaluate(fnc, passlist):
-
-    FNULL = open(os.devnull, 'w')
-    args = ["./com.sh", fnc] + passlist
-    out = subprocess.call(args, stdout=FNULL, stderr=subprocess.STDOUT)
-    f = open(os.path.join(os.path.dirname(__file__),"time.output"), 'rb')
-    score = int(f.readline())
+    score = 0
+    while (score == 0):
+        FNULL = open(os.devnull, 'w')
+        args = ["./com.sh", fnc] + passlist
+        out = subprocess.call(args, stdout=FNULL, stderr=subprocess.STDOUT)
+        f = open(os.path.join(os.path.dirname(__file__),"time.output"), 'rb')
+        score = int(f.readline())
 
     # For debug usage, can tell which passes are used and its score
-    #print "*************** ", args
-    #print "************** SCORE:", score
+    print "*************** ", args
+    print "************** SCORE:", score
     # Can comment out above part
 
     f.close()
